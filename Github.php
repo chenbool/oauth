8ea3a48351808eb01ae5f2e5829bce292a7bfe1d
bool <81001985@qq.com>
2018-09-30 17:18:37 +0800

    'github-oauth'

---
+++ Github.php
@@ -0,0 +1,67 @@
+<?php
+namespace extend\oauth;
+/**
+ * bool
+ * 30024167@qq.com
+ * Github oath登陆
+ * https://github.com/settings/developers
+ */
+class Github
+{
+	protected $client_id		=	'421746491968********';
+	protected $client_secret	=	'e613dea5ee6c9f8ece54*************';
+
+	function __construct(){
+	}
+
+	// 快捷方法 直接登陆
+	public function login($code){
+		$url = $this->get_access_token($code);
+		return $this->get_userinfo($url);
+	}
+
+	// 第一步:获取 access_token 地址
+	public function get_access_token($code){
+		$url = "https://github.com/login/oauth/access_token?client_id={$this->client_id}&client_secret={$this->client_secret}&code={$code}";
+		return 'https://api.github.com/user?'.file_get_contents($url);
+	}
+
+	// 第二步:获取用户信息
+	public function get_userinfo($url){
+		return json_decode( $this->curl_get($url), true);
+	}
+
+	// curl发送get请求
+    public function curl_get($url=''){
+    	
+        $ch = curl_init();
+        //设置选项，包括URL
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //不验证证书
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); //不验证证书
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+
+        //执行并获取HTML文档内容
+        $output = curl_exec($ch);
+
+        //释放curl句柄
+        curl_close($ch);
+
+        return $output;
+    }
+
+
+}
+
+/* 
+	//使用案例
+
+	use extend\oauth\Github;
+	$github = new Github();
+	$github->login($code);
+
+ */
